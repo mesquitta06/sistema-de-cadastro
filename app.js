b561e8febbeb670a8ff7f89cadbf15a07a444270
Otavio Ferreira De Mesquita <otaviom.prestserv@vibraenergia.com.br>
2023-09-16 20:03:10 -0300

    criando a função Alerta

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var teste1 = document.getElementById("");
 console.log("Rodando o sistema de Gerenciamento ");
 console.log("enviando dados...")
 
+var idAlerta= document.getElementById("DashboardName");
+
 function btnClica(){
     onclick();
+
+}
+
+function alertaCriado(){
+
 }
